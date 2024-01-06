f9520b0f4d3e70a80df4bd4f83bf04fc381e9870
Parthiban K <41236775+parthibankrpa@users.noreply.github.com>
2024-01-06 15:37:26 +0530

    with IP

--- server.js
+++ server.js
@@ -19,5 +19,28 @@ app.use(express.json());
 app.use("/mflix/users", mflixRoutes);
 
 app.listen(3000, () => {
+  printIP();
   console.log("Server started with 3000", process.env["USERNAME"]);
 });
+
+function printIP() {
+  const { networkInterfaces } = require("os");
+
+  const nets = networkInterfaces();
+  const results = Object.create(null); // Or just '{}', an empty object
+
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+      // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+      const familyV4Value = typeof net.family === "string" ? "IPv4" : 4;
+      if (net.family === familyV4Value && !net.internal) {
+        if (!results[name]) {
+          results[name] = [];
+        }
+        results[name].push(net.address);
+      }
+    }
+  }
+  console.log(JSON.stringify(results));
+}
